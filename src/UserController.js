0962783a48a8033564105f08cf64991ffd19f357
rodders1991 <georgeormrod@gmail.com>
2016-08-08 22:38:53 +0100

    New Styling for asthetically pleasing site

--- src/UserController.js
+++ src/UserController.js
@@ -1,1 +1,36 @@
 // JavaScript Document
+
+app.controller('UserCtrl', function($scope, userService) {
+	
+	//max 10 per page
+	$scope.currentPage = 0;
+    $scope.pageSize = 10;
+	$scope.userName = "Click a Avatar to view a User";
+	
+	
+	
+	$scope.searchUsers = function() { userService.getUsers($scope.query).then(function() {
+		
+		$scope.users = userService.users().items;
+		$scope.lastPage = $scope.users.length / $scope.pageSize;
+	});
+	};
+	
+	$scope.getUser = function(query) { userService.getUser(query).then(function() {
+		$scope.aUser = userService.user();
+		$scope.userName = userService.user().name;
+		$scope.userLocation = "From " + $scope.aUser.location;
+
+		});
+	};
+	
+	
+
+});
+
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
